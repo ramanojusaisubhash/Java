d5e5c59d0781b7b619807e3742480d1577cac6f2
ramanojusaisubhash <142982750+ramanojusaisubhash@users.noreply.github.com>
2023-09-04 11:46:31 +0530

    Update Average_V2.java

--- Average_V2.java
+++ Average_V2.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class Average{
+public class Average_V2{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter marks of 3 subjects: ");
