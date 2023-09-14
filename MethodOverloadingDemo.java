8f5cd00375c594ce13332750de25ece05bfa7549
ramanojusaisubhash <142982750+ramanojusaisubhash@users.noreply.github.com>
2023-09-14 16:30:44 +0530

    Create MethodOverloadingDemo.java

---
+++ MethodOverloadingDemo.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class Bank{
+    int getRateOfIntrest(){
+        return 0;
+    }
+}
+class SBI extends Bank{
+    int getRateOfIntrest(){
+        return 8;
+    }
+}
+class HDFC extends Bank{
+    int getRateOfIntrest(){
+        return 9;
+    }
+}
+class UNIONBANK extends Bank{
+    int getRateOfIntrest(){
+        return 10;
+    }
+}
+public class MethodOverloadingDemo{
+    public static void main(String args[]){
+       SBI s = new SBI();
+       HDFC h =new HDFC();
+       UNIONBANK u = new UNIONBANK();
+       System.out.println("SBI rate of intrest is : "+s.getRateOfIntrest());
+       System.out.println("HDFC rate of intrest is : "+h.getRateOfIntrest());
+       System.out.println("UNIONBANK rate of intrest is : "+u.getRateOfIntrest());
+    }
+}
