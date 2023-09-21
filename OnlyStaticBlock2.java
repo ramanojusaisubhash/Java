20b620eece29ce9f8058f3ea19a58acdcbb160f0
Ramanoju Sai Subhash <142982750+ramanojusaisubhash@users.noreply.github.com>
2023-09-21 15:34:34 +0530

    Create OnlyStaticBlock2.java

---
+++ OnlyStaticBlock2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class OnlyStaticBlock2{
+    static{
+         System.out.println("Inside static block");
+    }
+	public static void main(String args[]){
+		System.out.println("Inside main block");
+	}
+}
