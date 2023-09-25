82b12db08c8e3d99f5750ba05dc1e46b7fa1ec63
Ramanoju Sai Subhash <142982750+ramanojusaisubhash@users.noreply.github.com>
2023-09-25 09:59:13 +0530

    Create PrameterizeedConstructor.java

---
+++ PrameterizeedConstructor.java
@@ -0,0 +1,14 @@
+class Rectangle{
+    int a;
+    Rectangle(int l,int b){
+         a=l*b;
+    }
+}
+public class PrameterizeedConstructor{
+    public static void main(String args[]){
+        int x=Integer.parseInt(args[0]);
+        int y=Integer.parseInt(args[1]);
+        Rectangle R = new Rectangle(x,y);
+        System.out.println("Area of the rectangle is :"+R.a);
+    }
+}
