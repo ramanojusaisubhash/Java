3e600f35131558b45d1b23dd20dee1f299b20267
Ramanoju Sai Subhash <142982750+ramanojusaisubhash@users.noreply.github.com>
2023-09-25 21:02:35 -0700

    Create PolymorphismDemo.java

---
+++ PolymorphismDemo.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class Animal{
+    public void AnimalSound(){
+        System.out.println("An animal can make sounds");
+    }
+}
+class Cow extends Animal{
+    public void AnimalSound(){
+        System.out.println("A cow sounds - ambaa..");
+    }
+}
+class Cat extends Animal{ 
+    public void AnimalSound(){
+        System.out.println("A Cat sounds - Meow..");
+    }
+}
+public class PolymorphismDemo{
+    public static void main(String args[]){
+        Animal an = new Animal();
+        Animal mycow = new Cow();
+        Animal mycat = new Cat();
+        an.AnimalSound();
+        mycow.AnimalSound();
+        mycat.AnimalSound();
+    }
+}
