e6654ccd3d43d89339f1b162b5eb82c4d65168af
Ramanoju Sai Subhash <142982750+ramanojusaisubhash@users.noreply.github.com>
2023-10-19 16:24:37 +0530

    Create VectorsDemo.java

---
+++ VectorsDemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class {
+    public static void main(String args[]){
+        Vector<String> v = new <String> Vector();
+        v.add("sai");
+        v.add("Ram");
+        v.add("raj");
+        v.addElement("Mohan");
+        System.out.println(v);
+    }
+}
